e43116f654b571fd7296eee5fbeb21c2027d64c9
GlenLuz <111454200+GlenLuz@users.noreply.github.com>
2022-11-21 22:14:53 -0500

    Update and rename Main Project to MainProject.py
    Board is created and I can see it but it closes after 1 second in VS. Need to figure out how to keep it open for an indefinite period of time.

---
+++ MainProject.py
@@ -0,0 +1,14 @@
+import turtle
+import time
+import random
+
+delay = 0.1
+score = 0
+high_score = 0
+ 
+# Board for the game
+window = turtle.Screen()
+window.title("Python Game")
+window.bgcolor('white')
+window.setup(width=600, height=600)
+window.tracer(0)

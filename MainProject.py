80e724124337969980ab0e57ed727ce870b78627
GlenLuz <111454200+GlenLuz@users.noreply.github.com>
2022-12-05 07:48:33 -0500

    Update MainProject.py
    Added the Score Board to the window and movement for the snake

--- MainProject.py
+++ MainProject.py
@@ -29,4 +29,52 @@ apple.speed()
 apple.penup()
 apple.goto(0, 100)
 
+#Score Board
+pen = turtle.Turtle()
+pen.speed(0)
+pen.shape("square")
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 250)
+pen.write("Score : 0 High Score : 0", align="center",
+		font=("ariel", 24, "bold"))
+
+#Movement
+def group(): 
+    if snake.direction != "down":
+        snake.direction = "up"
+
+def movedown():
+    if snake.direction != "up":
+        snake.direction = "down"
+
+def moveleft(): 
+    if snake.direction != "right":
+        snake.direction = "left"
+
+def moveright():
+    if snake.direction != "left":
+        snake.direction = "right"
+
+def move():
+    if snake.direction == "up":
+        y = snake.ycor()
+        snake.sety(y+20)
+    if snake.direction == "down":
+        y = snake.ycor()
+        snake.sety(y-20)
+    if snake.direction == "left":
+        x = snake.xcor()
+        snake.setx(x-20)
+    if snake.direction == "right":
+        x = snake.xcor()
+        snake.setx(x+20)
+
+window.listen()
+window.onkeypress(group, "w")
+window.onkeypress(group, "s")
+window.onkeypress(group, "a")
+window.onkeypress(group, "d")
+
 turtle.done()
